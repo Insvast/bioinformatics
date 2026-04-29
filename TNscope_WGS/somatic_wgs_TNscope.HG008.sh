ffbc47ded721da9a0bad0a17b795b2bf133f575c
biowlys <xyli@insvast.com>
2026-04-29 15:57:18 +0800

    add TNscope WGS scripts

---
+++ TNscope_WGS/somatic_wgs_TNscope.HG008.sh
@@ -0,0 +1,192 @@
+#!/bin/sh
+#SBATCH --job-name=WL-TNscope-HG008
+#SBATCH --partition=deimos
+#SBATCH --cpus-per-task=64
+#SBATCH -o %x-%j.log
+#SBATCH -e %x-%j.log
+
+# Copyright (c) 2016-2024 Sentieon Inc. All rights reserved
+
+# *******************************************
+# Script to perform TNscope variant calling on
+# samples up to 50x coverage using a matched 
+# paired Tumor+normal sample with fastq files 
+# named normal_1.fastq.gz, normal_2.fastq.gz
+# tumor_1.fastq.gz, tumor_2.fastq.gz
+# *******************************************
+
+set -eu
+# set -xeuo pipefail
+
+export SENTIEON_INSTALL_DIR=/APP/u22/x86_com/sentieon/202503.03/sentieon-genomics-202503.03
+export LD_LIBRARY_PATH=$SENTIEON_INSTALL_DIR/lib
+export SENTIEON_LICENSE=IP:8091 #or using licsrvr: c1n11.sentieon.com:5443，or /home/Licenses/Sentieon.lic
+export LD_PRELOAD=/miniconda3/envs/anasys/lib/libjemalloc.so.2
+export PATH=$SENTIEON_INSTALL_DIR/bin:$PATH
+
+
+# Update with the fullpath location of your sample fastq
+TUMOR_SM="HG008-T" #sample name
+TUMOR_RGID="rg_$TUMOR_SM" #read group ID
+NORMAL_SM="HG008-N" #sample name
+NORMAL_RGID="rg_$NORMAL_SM" #read group ID
+PL="ILLUMINA" #or other sequencing platform
+FASTQ_FOLDER="/project/TumorNormal-test/data"
+TUMOR_FASTQ_1="$FASTQ_FOLDER/HG008-T_Illumina_R1.fastq.gz"
+TUMOR_FASTQ_2="$FASTQ_FOLDER/HG008-T_Illumina_R2.fastq.gz" #If using Illumina paired data
+NORMAL_FASTQ_1="$FASTQ_FOLDER/HG008-N_Illumina_R1.fastq.gz"
+NORMAL_FASTQ_2="$FASTQ_FOLDER/HG008-N_Illumina_R2.fastq.gz" #If using Illumina paired data
+
+# Update with the location of the reference data files
+FASTA_DIR="/reference/human/pangemone"
+FASTA="$FASTA_DIR/hg38_ucsc.fa"
+KNOWN_DBSNP="$FASTA_DIR/Homo_sapiens_assembly38.dbsnp138.vcf.gz"
+PON=  # the TNscope panel-of-normals VCF, 没有就设置为空
+
+# Update with the location of the Sentieon software package and license file
+
+
+# Other settings
+NT=$(nproc) #number of threads to use in computation, set to number of cores in the server
+START_DIR="$PWD" #Determine where the output files will be stored
+BCFTOOLS_BINARY="/miniconda3/envs/anasys/bin/bcftools" #bcftools >=1.7 is recommended
+MIN_QUAL=10
+
+
+
+
+
+# You do not need to modify any of the lines below unless you want to tweak the pipeline
+
+# ************************************************************************************************************************************************************************
+
+# ******************************************
+# 0. Setup
+# ******************************************
+WORKDIR="$START_DIR"
+mkdir -p $WORKDIR
+LOGFILE=$WORKDIR/run.log
+exec >$LOGFILE 2>&1
+cd $WORKDIR
+
+echo ">>> 开始时间：$(date "+%Y-%m-%d %H:%M:%S")"
+
+# ******************************************
+# 1a. Mapping reads with BWA-MEM, sorting for tumor sample
+# ******************************************
+#The results of this call are dependent on the number of threads used. To have number of threads independent results, add chunk size option -K 10000000 
+( $SENTIEON_INSTALL_DIR/bin/sentieon bwa mem -R "@RG\tID:$TUMOR_RGID\tSM:$TUMOR_SM\tPL:$PL" \
+    -t $NT -K 10000000 $FASTA $TUMOR_FASTQ_1 $TUMOR_FASTQ_2 || \
+    { echo -n 'BWA error'; exit 1; } ) | \
+    $SENTIEON_INSTALL_DIR/bin/sentieon util sort -o tumor_sorted.bam -t $NT --sam2bam -i - || \
+    { echo "Alignment1 failed"; exit 1; }
+
+# ******************************************
+# 1b. Mapping reads with BWA-MEM, sorting for normal sample
+# ******************************************
+#The results of this call are dependent on the number of threads used. To have number of threads independent results, add chunk size option -K 10000000 
+( $SENTIEON_INSTALL_DIR/bin/sentieon bwa mem -R "@RG\tID:$NORMAL_RGID\tSM:$NORMAL_SM\tPL:$PL" \
+    -t $NT -K 10000000 $FASTA $NORMAL_FASTQ_1 $NORMAL_FASTQ_2 || \
+    { echo -n 'BWA error'; exit 1; } ) | \
+    $SENTIEON_INSTALL_DIR/bin/sentieon util sort -o normal_sorted.bam -t $NT --sam2bam -i - || \
+    { echo "Alignment2 failed"; exit 1; }
+
+# ******************************************
+# 2a. Metrics for tumor sample
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT -i tumor_sorted.bam \
+    --algo MeanQualityByCycle tumor_mq_metrics.txt \
+    --algo QualDistribution tumor_qd_metrics.txt --algo GCBias \
+    --summary tumor_gc_summary.txt tumor_gc_metrics.txt --algo AlignmentStat \
+    --adapter_seq '' tumor_aln_metrics.txt \
+    --algo InsertSizeMetricAlgo tumor_is_metrics.txt \
+    --algo CoverageMetrics tumor_coverage_metrics || \
+    { echo "Metrics1 failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon plot GCBias -o tumor_gc-report.pdf tumor_gc_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot QualDistribution \
+    -o tumor_qd-report.pdf tumor_qd_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot MeanQualityByCycle \
+    -o tumor_mq-report.pdf tumor_mq_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot InsertSizeMetricAlgo \
+    -o tumor_is-report.pdf tumor_is_metrics.txt
+
+# ******************************************
+# 2b. Metrics for normal sample
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT -i normal_sorted.bam \
+    --algo MeanQualityByCycle normal_mq_metrics.txt \
+    --algo QualDistribution normal_qd_metrics.txt --algo GCBias \
+    --summary normal_gc_summary.txt normal_gc_metrics.txt --algo AlignmentStat \
+    --adapter_seq '' normal_aln_metrics.txt \
+    --algo InsertSizeMetricAlgo normal_is_metrics.txt \
+    --algo CoverageMetrics normal_coverage_metrics || \
+    { echo "Metrics2 failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon plot GCBias -o normal_gc-report.pdf normal_gc_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot QualDistribution \
+    -o normal_qd-report.pdf normal_qd_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot MeanQualityByCycle \
+    -o normal_mq-report.pdf normal_mq_metrics.txt
+$SENTIEON_INSTALL_DIR/bin/sentieon plot InsertSizeMetricAlgo \
+    -o normal_is-report.pdf normal_is_metrics.txt
+
+# ******************************************
+# 3a. Remove Duplicate Reads for tumor
+# sample. It is possible
+# to remove instead of mark duplicates
+# by adding the --rmdup option in Dedup
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i tumor_sorted.bam --algo LocusCollector \
+    --fun score_info tumor_score.txt || { echo "LocusCollector1 failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i tumor_sorted.bam --algo Dedup \
+    --score_info tumor_score.txt --metrics tumor_dedup_metrics.txt tumor_deduped.bam || \
+    { echo "Dedup1 failed"; exit 1; }
+
+# ******************************************
+# 3b. Remove Duplicate Reads for normal
+# sample. It is possible
+# to remove instead of mark duplicates
+# by adding the --rmdup option in Dedup
+# ******************************************
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i normal_sorted.bam --algo LocusCollector \
+    --fun score_info normal_score.txt || { echo "LocusCollector2 failed"; exit 1; }
+
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -t $NT -i normal_sorted.bam --algo Dedup \
+    --score_info normal_score.txt --metrics normal_dedup_metrics.txt normal_deduped.bam || \
+    { echo "Dedup2 failed"; exit 1; }
+
+# ******************************************
+# 4. Somatic and Structural variant calling
+# ******************************************
+# Consider adding `--disable_detector sv --trim_soft_clip` if not interested in SV calling
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r $FASTA -t $NT -i tumor_deduped.bam \
+    -i normal_deduped.bam \
+    --algo TNscope --tumor_sample $TUMOR_SM --normal_sample $NORMAL_SM \
+    --dbsnp $KNOWN_DBSNP ${PON:+--pon $PON} output_tnscope.pre_filter.vcf.gz || \
+    { echo "TNscope failed"; exit 1; }
+
+# ******************************************
+# 5. Variant filtration
+# ******************************************
+( $BCFTOOLS_BINARY annotate -x "FILTER/triallelic_site" output_tnscope.pre_filter.vcf.gz || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "insignificant" -e "(PV>0.25 && PV2>0.25) || (INFO/STR == 1 && PV>0.05)" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "orientation_bias" -e "FMT/FOXOG[0] == 1" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "strand_bias" -e "SOR > 3" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "low_qual" -e "QUAL < $MIN_QUAL" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "short_tandem_repeat" -e "RPA[0]>=10" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "noisy_region" -e "ECNT>5" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+    ( $BCFTOOLS_BINARY filter -m + -s "read_pos_bias" -e "FMT/ReadPosRankSumPS[0] < -8" || \
+        { echo "VCF filtering failed"; exit 1; } ) | \
+	$SENTIEON_INSTALL_DIR/bin/sentieon util vcfconvert - output_tnscope.filtered.vcf.gz || \
+        { echo "VCF filtering failed"; exit 1; }
+
+echo ">>> 结束时间：$(date "+%Y-%m-%d %H:%M:%S")"
